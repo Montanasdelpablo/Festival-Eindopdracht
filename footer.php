a261b1c49420f53b9ff402c926bcabc63f4d372d
Pablo Cleij <pablocley@hotmail.com>
2016-04-29 12:12:20 +0200

    Added main structure Footer

--- footer.php
+++ footer.php
@@ -1,8 +1,34 @@
-	<footer>
+	<footer class="footer">
+		<div id="footercontent" class="container">
+
+			<ul class="col-md-4">
+				<h2> Contact </h2>
+				<li> Duinkerkenstraat 50 </li>
+				<li> 9723 BT</li>
+				<li> Groningen</li>
+			</ul>
+			<div id="socialmedia">
+			<ul  class="col-md-4">
+				<h2> Social Media </h2>
+				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/social.png"></a>   </li>
+				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/social-1.png"></a>  </li>
+				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/social-network.png"></a>   </li>
+				
+			</ul>
+			</div>
+
+			<ul class="col-md-4">
+				<h2> Made by </h2>
+				<li> Pablo Montanas</li>
+				<li> Website:</li>
+				<li> <a href="#"> pablomontanas.com </a> </li>
+			</ul>
 		
+		</div>
 
 		<script type="text/javascript" src="<?php echo bloginfo('template_url')?>/js/bootstrap.min.js"></script>
 
+		<?php wp_footer(); ?>
 	</footer>
 
 
