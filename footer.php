fbbdb86a2ec07fba9d7e9776136fbb9255d9b361
Pablo Cleij <pablocley@hotmail.com>
2016-05-18 00:06:02 +0200

    Footer & Header restructured

--- footer.php
+++ footer.php
@@ -1,29 +1,30 @@
 	<footer class="footer">
 		<div id="footercontent" class="container">
-
-			<ul class="col-md-4">
+		<ul class="row"> 
+			<ul class="col-lg-4">
 				<h2> Contact </h2>
 				<li> Duinkerkenstraat 50 </li>
 				<li> 9723 BT</li>
 				<li> Groningen</li>
 			</ul>
+			
+			<ul class="col-lg-4">
 			<div id="socialmedia">
-			<ul class="col-md-4">
 				<h2> Social Media </h2>
-				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/"></a>   </li>
-				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/"></a>  </li>
-				<li><a href="#"><img src="<?php echo bloginfo('template_url')?>/img/"></a>   </li>
-				
+				<li><a href="#"></a>   </li>
+				<li><a href="#"></a>  </li>
+				<li><a href="#"></a>   </li>
+			</div>	
 			</ul>
-			</div>
+			
 
-			<ul class="col-md-4">
+			<ul class="col-lg-4">
 				<h2> Made by </h2>
 				<li> </li>
 				<li> Website:</li>
 				<li> <a href="#"> </a> </li>
 			</ul>
-		
+		</ul>
 		</div>
 
 		<script type="text/javascript" src="<?php echo bloginfo('template_url')?>/js/bootstrap.min.js"></script>
